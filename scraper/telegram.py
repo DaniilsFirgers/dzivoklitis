1e7597fccf972a21b286a0507f7825dbb4c6d08a
Daniils Firgers <dfirger@gmail.com>
2024-11-10 16:48:59 +0200

    now can handle favorite messaeg adding and sending back

--- scraper/telegram.py
+++ scraper/telegram.py
@@ -1,13 +1,34 @@
+import time
 import telebot
 from telebot import types
 
 from scraper.flat import Flat
+from scraper.database import FlatsTinyDb
+from threading import Thread
 
 
 class TelegramBot:
-    def __init__(self, token, chat_id):
+    def __init__(self, token, chat_id, tiny_db: FlatsTinyDb):
         self.bot = telebot.TeleBot(token)
         self.chat_id = chat_id
+        self.tiny_db = tiny_db
+
+        self.bot.callback_query_handler(func=lambda call: call.data.startswith(
+            "add_to_favorites:"))(self.handle_add_to_favorites)
+        self.bot.message_handler(commands=["favorites"])(self.send_favorites)
+        Thread(target=self.start_polling, daemon=True).start()
+
+    def handle_add_to_favorites(self, call):
+        id = call.data.split(":")[1]
+        if self.tiny_db.exists(id, "favorites"):
+            return self.bot.answer_callback_query(
+                call.id, "This flat is already in your favorites list ✅")
+
+        flat = self.tiny_db.get(id, "parsed")
+        self.tiny_db.insert(id, flat["flat"], "favorites")
+        self.bot.answer_callback_query(
+            call.id, "Flat added to favorites ❤️"
+        )
 
     def send_message(self, message: str):
         self.bot.send_message(
@@ -16,9 +37,46 @@ class TelegramBot:
             parse_mode="Markdown"
         )
 
-    def send_flat_message(self, flat: Flat, id: str):
-        msg_txt = (
-            f"#: {id}\n"
+    def send_favorites(self, _):
+        favorites = self.tiny_db.favorites_db.all()
+        if not favorites:
+            return self.bot.send_message(
+                chat_id=self.chat_id,
+                text="You don't have any favorites yet 😢"
+            )
+
+        for counter, favorite in enumerate(favorites, start=1):
+            flat_obj = Flat(**favorite["flat"])
+            flat_msg = self.flat_to_msg(flat_obj, counter)
+            self.bot.send_message(
+                chat_id=self.chat_id,
+                text=flat_msg,
+                parse_mode="Markdown"
+            )
+            time.sleep(0.5)
+
+    def send_flat_message(self, flat: Flat, counter: str):
+        msg_txt = self.flat_to_msg(flat, counter)
+
+        markup = types.InlineKeyboardMarkup()
+        markup.add(
+            types.InlineKeyboardButton(
+                "🔍 View Link", url=flat.link),
+        )
+        markup.add(
+            types.InlineKeyboardButton(
+                "❤️ Add to Favorites", callback_data=f"add_to_favorites:{flat.id}"),
+        )
+
+        self.bot.send_message(
+            chat_id=self.chat_id,
+            text=msg_txt,
+            parse_mode="Markdown",
+            reply_markup=markup
+        )
+
+    def flat_to_msg(self, flat: Flat, counter: int = None) -> str:
+        base_msg = (
             f"*District*: {flat.district}\n"
             f"*Street*: {flat.street}\n"
             f"*Series*: {flat.series}\n"
@@ -29,15 +87,7 @@ class TelegramBot:
             f"*Full price*: {flat.full_price}\n"
         )
 
-        markup = types.InlineKeyboardMarkup()
-        markup.add(
-            types.InlineKeyboardButton(
-                "🔍 View Link", url=flat.link),
-        )
+        return f"#: {counter}\n" + base_msg if counter is not None else base_msg
 
-        self.bot.send_message(
-            chat_id=self.chat_id,
-            text=msg_txt,
-            parse_mode="Markdown",
-            reply_markup=markup
-        )
+    def start_polling(self):
+        self.bot.polling()
