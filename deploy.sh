959f370854bf714d97bad0e6767cde7adcfb8ced
Daniils Firgers <dfirger@gmail.com>
2024-11-13 22:40:07 +0200

    created deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+echo "[>] Starting Deployment..."
+
+compose_file=docker-compose.yml
+
+echo "[+] Removing containers, volume, images and networks older than 1 week..."
+docker system prune --force --filter "until=168h" --all
+
+echo "[+] Stopping any running containers from docker-compose..."
+docker-compose down
+
+echo "[+] Starting new container using docker-compose in detached mode..."
+docker-compose up -d
+
+if [[ $? -eq 0 ]]; then
+    echo "[✔] Deployment successful!"
+else
+    echo "[!] Failed to start the container."
+    exit 1
+fi
