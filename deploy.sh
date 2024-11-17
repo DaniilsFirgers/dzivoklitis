488d83fd9a2f98636622270a916e87d76331dc10
Daniils Firgers <dfirger@gmail.com>
2024-11-17 10:58:48 +0200

    now pulling latest images in deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ docker system prune --force --filter "until=168h" --all
 echo "[+] Stopping any running containers from docker compose..."
 docker compose down
 
+echo "[+] Pulling the latest images..."
+docker compose pull
+
 echo "[+] Starting new container using docker compose in detached mode..."
 docker compose up -d
 
