3db7a14a981905612491d6b81b84d8d31bf26148
Daniils Firgers <dfirger@gmail.com>
2024-11-24 21:57:44 +0200

    working on rabbitmq client

--- scraper/rabbitmq/client.py
+++ scraper/rabbitmq/client.py
@@ -1,13 +1,15 @@
 import pika
 from scraper.config import RabbitMq
 import json
+from scraper.flat import Flat
 from scraper.rabbitmq.types import RabbitMqAction, actions
 from threading import Thread, local
 from scraper.database import FlatsTinyDb
+from scraper.telegram import TelegramBot
 
 
 class RabbitMqClient:
-    def __init__(self, config: RabbitMq, db: FlatsTinyDb):
+    def __init__(self, config: RabbitMq, db: FlatsTinyDb, telegram: TelegramBot):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(
             host=config.host, port=config.port
         ))
@@ -15,12 +17,15 @@ class RabbitMqClient:
         self.producer_channel = self.connection.channel()
         self.consumer_channel = self.connection.channel()
 
+        # TODO: implement try-except block for connection
+
         # declare exchange
         self.producer_channel.exchange_declare(
             exchange=self.exchange, exchange_type="topic", durable=True)
         self.consumer_channel.exchange_declare(
             exchange=self.exchange, exchange_type='topic', durable=True)
         self.db = db
+        self.telegram = telegram
         self._actions = actions.values()
         self._local = local()
 
@@ -41,54 +46,78 @@ class RabbitMqClient:
                 exchange=self.exchange, queue=action["queue_name"], routing_key=action["routing_key"])
             self.consumer_channel.queue_bind(
                 exchange=self.exchange, queue=action["queue_name"], routing_key=action["routing_key"]
+
             )
 
-    def publish(self, action: RabbitMqAction, message: dict | str):
-        if isinstance(message, dict):
-            message_body = json.dumps(message)
+    def publish(self, action: RabbitMqAction, message: Flat | str):
+        if isinstance(message, Flat):
+            flat_dict = message.to_dict()
+            message_body = json.dumps(flat_dict)
             content_type = 'application/json'
         else:
             message_body = str(message)
             content_type = 'text/plain'
-        self.producer_channel.basic_publish(
-            exchange=self.exchange,
-            routing_key=action["routing_key"],
-            body=message_body,
-            properties=pika.BasicProperties(
-                content_type=content_type,
-                delivery_mode=2,  # make message persistent
+
+        self.connection.add_callback_threadsafe(
+            lambda: self.producer_channel.basic_publish(
+                exchange=self.exchange,
+                routing_key=action["routing_key"],
+                body=message_body,
+                properties=pika.BasicProperties(
+                    content_type=content_type,
+                    delivery_mode=2  # Make the message persistent
+                )
             )
         )
 
     def _callback(self, queue_name: str):
         match queue_name:
             case "add_flat":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
-                return callback
-            case "delete_flat":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    print(f" [!] Raw body received: {body}")
+                    try:
+                        flat_obj = json.loads(body.decode('utf-8'))
+                        flat_instance = Flat(**flat_obj)
+                        print(f" [x] Received 'add_flat' {flat_instance.id}")
+                    except json.JSONDecodeError as e:
+                        print(f" [!] Failed to decode JSON: {e}")
                 return callback
             case "add_favorite_flat":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    flat_id = body.decode('utf-8')
+                    print(
+                        f" [x] Received 'add_favorite_flat' {flat_id}")
+
                 return callback
             case "delete_favorite_flat":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    flat_id = body.decode('utf-8')
+                    print(
+                        f" [x] Received 'delete_favorite_flat' {flat_id}")
                 return callback
             case "delete_old_flats":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    try:
+                        flat_obj = json.loads(body.decode('utf-8'))
+                        flat_instance = Flat(**flat_obj)
+                        print(
+                            f" [x] Received 'delete_old_flats' {flat_instance.id}")
+                    except json.JSONDecodeError as e:
+                        print(f" [!] Failed to decode JSON: {e}")
                 return callback
             case "send_text_message":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    decoded_body = body.decode('utf-8')
+                    self.telegram.send_message(decoded_body)
                 return callback
             case "send_flat_message":
-                def callback(ch, method, properties, body):
-                    print(f" [x] Received {body}")
+                def callback(ch, method, properties, body: bytes):
+                    try:
+                        flat_obj = json.loads(body.decode('utf-8'))
+                        flat_instance = Flat(**flat_obj)
+                        self.telegram.send_flat_message(flat_instance)
+                    except json.JSONDecodeError as e:
+                        print(f" [!] Failed to decode JSON: {e}")
                 return callback
         return callback
 
