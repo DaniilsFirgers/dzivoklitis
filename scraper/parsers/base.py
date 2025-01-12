7be0719530a1194dcc85c5cd986f2a716ea3095b
Daniils Firgers <dfirger@gmail.com>
2025-01-12 12:13:08 +0200

    created base Class for scraper

---
+++ scraper/parsers/base.py
@@ -0,0 +1,22 @@
+from typing import List
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+class BaseParser:
+    def __init__(self, name: str, scheduler: BackgroundScheduler, deal_type: str, sleep_time: int = 3):
+        self.name = name
+        self.scheduler = scheduler
+        self.deal_type = deal_type
+        self.sleep_time = sleep_time
+
+    def run(self):
+        districts = self.get_districts_list()
+        self.scrape(districts)
+        self.scheduler.add_job(
+            self.scrape, "cron", hour="9,12,15,18,21", minute=0, args=[districts])
+
+    def get_districts_list(self) -> List[int]:
+        raise NotImplementedError
+
+    def scrape(self, districts: List[int]) -> None:
+        raise NotImplementedError
