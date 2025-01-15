7ed0cb2e92827d6f4819fe33d1773832edce3c44
Daniils Firgers <dfirger@gmail.com>
2025-01-15 22:19:16 +0200

    added try_parse_int and try_parse_float to meta.py utils

--- scraper/utils/meta.py
+++ scraper/utils/meta.py
@@ -10,3 +10,17 @@ class SingletonMeta(type):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def try_parse_int(value: str) -> int:
+    try:
+        return int(value)
+    except ValueError:
+        return 0
+
+
+def try_parse_float(value: str) -> float:
+    try:
+        return float(value)
+    except ValueError:
+        return 0.0
