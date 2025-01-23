98f502d17533fffe95399d02f53f0d3bee4b8bdf
Daniils Firgers <dfirger@gmail.com>
2025-01-23 22:47:42 +0200

    working on ss.py scraper

--- scraper/parsers/ss.py
+++ scraper/parsers/ss.py
@@ -2,33 +2,36 @@ import time
 from typing import Dict, List
 from bs4 import BeautifulSoup, ResultSet, Tag
 import requests
-from scraper.config import District, GeneralConfig
+from scraper.config import District, SsParserConfig
 from scraper.core.postgres import Postgres, Type
 from scraper.core.telegram import TelegramBot
-from scraper.flat import Flat, SS_Flat
+from scraper.flat import Coordinates, Flat, SS_Flat
 from scraper.parsers.base import BaseParser
 from scraper.utils.logger import logger
 from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+from geopy.geocoders import Nominatim
 
 
 class SSParser(BaseParser):
-    def __init__(self, source: str, scheduler: BackgroundScheduler, telegram_bot: TelegramBot, postgres: Postgres,
-                 districts: List[District], general_config: GeneralConfig
+    def __init__(self, scheduler: BackgroundScheduler, telegram_bot: TelegramBot, postgres: Postgres,
+                 districts: List[District], config: SsParserConfig, sleep_time: int
                  ):
-        super().__init__(source, scheduler,
-                         general_config.deal_type, general_config.message_sleep)
-        self.city_name = general_config.city_name
+
+        super().__init__(config.name, scheduler,
+                         config.deal_type, sleep_time)
+        self.city_name = config.city_name
         self.districts = districts
-        self.look_back_arg = general_config.look_back_argument
+        self.look_back_arg = config.timeframe
         self.telegram_bot = telegram_bot
         self.postgres = postgres
 
     def get_districts_list(self) -> List[int]:
         districts_found = []
-        link = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/"
+        url = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/"
 
         try:
-            response = requests.get(link, timeout=10)
+            response = requests.get(url, timeout=10)
             response.raise_for_status()
         except requests.RequestException as e:
 
@@ -65,8 +68,8 @@ class SSParser(BaseParser):
     def scrape(self, districts: List[District]) -> None:
         flats_found: Dict[str, List[Flat]] = {}
         for district in districts:
-            pages_link = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/{district.name}/{self.look_back_arg}/{self.deal_type}/"
-            response = requests.get(pages_link)
+            pages_url = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/{district.name}/{self.look_back_arg}/{self.deal_type}/"
+            response = requests.get(pages_url)
             bs = BeautifulSoup(response.text, "html.parser")
             all_pages: ResultSet[Tag] = bs.find_all(name="a", class_="navi")
 
@@ -76,39 +79,63 @@ class SSParser(BaseParser):
                 pages.append(page)
 
             for page in pages:
-                page_link = f"https://www.ss.lv/en/real-estate/flats/riga/{district.name}/{self.look_back_arg}/{self.deal_type}/page{page}.html"
-                page_res = requests.get(page_link)
+                page_url = f"https://www.ss.lv/real-estate/flats/riga/{district.name}/{self.look_back_arg}/{self.deal_type}/page{page}.html"
+                page_res = requests.get(page_url)
                 bs = BeautifulSoup(page_res.text, "html.parser")
 
                 descriptions: ResultSet[Tag] = bs.find_all(
                     name="a", class_="am")
                 streets: ResultSet[Tag] = bs.find_all(
                     name="td", class_="msga2-o pp6")
+                image_urls: ResultSet[Tag] = bs.find_all(
+                    name="img", class_="isfoto foto_list")
 
                 # extracting all data about a flat
                 for i in range(0, len(streets), 7):
                     # Get a chunk of 7 streets
                     description = descriptions[int(i/7)]
                     info = description.get("href")
-                    link = f"https://www.ss.lv/{info}"
+                    url = f"https://www.ss.lv/{info}"
                     id = description.get("id")
-
-                    if self.postgres.check_if_exists(id, Type.FLATS):
-                        continue
 
                     chunk: list[Tag] = streets[i:i + 7]
                     raw_info = [street.get_text() for street in chunk]
+                    img_url = self.get_image_url(image_urls, i)
 
-                    flat = SS_Flat(id, link, district, raw_info)
-                    flat.create()
+                    flat = SS_Flat(url, district, raw_info, img_url)
+
+                    # Possible options here:
+                    # 1. Flat with this id and price is already in the database -> skip
+                    # 2. Flat with this id is already in in the database, but
+                    # the price has changed -> update the price and updated_at and
+                    # add a new flat_price_update record
+                    # 3. Flat with this id is not in the database:
+                    #   a. Flat with the same parameters (price, area, rooms, etc.)
+                    #   exists in the database and price_per_m2 is the same, but with a different id -> update
+                    #   b. Flat with the same parameters (price, area, rooms, etc.) exists in the database,
+                    #   but price_per_m2 is different -> update the price and updated_at and add a new flat_price_update record
+                    #   c. Flat with the same parameters (price, area, rooms, etc.) does not exist in the database -> add
+
+                    # If flat with the same id and price_per_m2 exists in the database, skip it
+                    if self.postgres.exists_with_id_and_price(flat.id, flat.price_per_m2):
+                        continue
 
                     try:
                         flat.validate()
                     except ValueError:
                         continue
 
+                    coordinates = self.get_coordinates(
+                        self.city_name, flat.street)
+
+                    if coordinates is not None:
+                        flat.add_coordinates(coordinates)
+
+                    if img_url is not None:
+                        flat.load_img()
+
                     try:
-                        self.postgres.add(flat)
+                        self.postgres.add_or_update(flat)
                         logger.info(f"Added flat {flat.id} to the database")
                     except Exception as e:
                         logger.error(e)
@@ -128,3 +155,26 @@ class SSParser(BaseParser):
                 msg = f"*{index}/{len(flats)}*"
                 self.telegram_bot.send_flat_message(flat, Type.FLATS, msg)
                 time.sleep(self.sleep_time)
+
+    def get_image_url(self, image_urls: ResultSet[Tag], i: int) -> str | None:
+        if len(image_urls) == 0:
+            return None
+        if int(i/7) >= len(image_urls):
+            return None
+        img_url: str = image_urls[int(i/7)].get("src")
+        return img_url
+
+    def get_coordinates(self, city: str, street: str) -> Coordinates | None:
+        geolocator = Nominatim(user_agent="flats_scraper")
+        # TODO: remove .strip() when the bug with geopy is fixed
+        location = geolocator.geocode({
+            "street": street,
+            "city": city,
+            "country": "Latvia"
+        })
+
+        if location is None:
+            print(f"Could not get coordinates for {street}")
+            return None
+
+        return Coordinates(location.latitude, location.longitude)
