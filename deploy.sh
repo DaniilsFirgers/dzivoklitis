98736ef9afc4f09e677f82b49f790534a17e8889
Daniils Firgers <dfirger@gmail.com>
2025-01-26 11:16:47 +0200

    updated deploy.sh script to delete nedeed volumes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 echo "[>] Starting Deployment..."
 
 compose_file=docker-compose.yml
+protected_volume="postgres_data"
 
-echo "[+] Stopping and removing containers, networks, and volumes related to the compose file..."
-docker compose down --volumes --remove-orphans
+echo "[+] Stopping and removing containers, networks, and orphans related to the compose file..."
+docker compose down --remove-orphans
 
 echo "[+] Removing dangling images (unused by any container)..."
 docker image prune --force
@@ -11,8 +14,8 @@ docker image prune --force
 echo "[+] Removing unused networks..."
 docker network prune --force
 
-echo "[+] Removing unused volumes..."
-docker volume prune --force
+echo "[+] Removing unused volumes except for $protected_volume..."
+docker volume ls -q | grep -v "$protected_volume" | xargs -r docker volume rm
 
 echo "[+] Pulling the latest images..."
 docker compose pull
