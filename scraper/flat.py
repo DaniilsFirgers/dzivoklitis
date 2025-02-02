d63ba47b85941f208c3a79221ad7d9b58497a887
Daniils Firgers <dfirger@gmail.com>
2025-02-02 20:52:32 +0200

    adjustments made to flat class to take into account unicersal structure

--- scraper/flat.py
+++ scraper/flat.py
@@ -20,16 +20,16 @@ class Flat():
     url: str
     district: str
     source: Source
+    deal_type: str = field(default=None)
     id: str = field(default=None)
-    district_info: Optional[District] = field(default=None)
-    price_per_m2: Optional[int] = field(default=None)
+    price: Optional[int] = field(default=None)
     rooms: Optional[int] = field(default=None)
     street: Optional[str] = field(default=None)
     area: Optional[float] = field(default=None)
     floor: Optional[int] = field(default=None)
     floors_total: Optional[int] = field(default=None)
     series: Optional[str] = field(default=None)
-    full_price: Optional[int] = field(default=None)
+    price_per_m2: Optional[int] = field(default=None)
     latitude: Optional[float] = field(default=None)
     longitude: Optional[float] = field(default=None)
     image_data: Optional[bytes] = field(default=None)
@@ -41,8 +41,6 @@ class Flat():
         pass
 
     def validate(self):
-        if not self.district_info:
-            raise ValueError("District info is not set")
         pass
 
     def add_coordinates(self, coordinates: Coordinates):
@@ -64,7 +62,7 @@ class Flat():
         For a more efficient storage, we hash the id with md5.
         """
         return hashlib.md5(
-            f"{self.source.value}-{self.district}-{self.street}-{self.series}-{self.rooms}-{self.area}-{self.floor}-{self.floors_total}".encode()).hexdigest()
+            f"{self.source.value}-{self.deal_type}-{self.district}-{self.street}-{self.series}-{self.rooms}-{self.area}-{self.floor}-{self.floors_total}".encode()).hexdigest()
 
     @classmethod
     def from_sql_row(cls, *row):
@@ -78,32 +76,35 @@ class Flat():
         return cls(
             id=row[0],
             source=row[1],
-            url=row[2],
-            district=row[3],
-            street=row[4],
-            rooms=row[5],
-            floors_total=row[6],
-            floor=row[7],
-            area=row[8],
-            series=row[9],
-            full_price=int(row[13] * row[8]),
-            price_per_m2=row[13],
-            latitude=row[10],
-            longitude=row[11],
-            image_data=row[12]
+            deal_type=row[2],
+            url=row[3],
+            district=row[4],
+            street=row[5],
+            rooms=row[6],
+            floors_total=row[7],
+            floor=row[8],
+            area=row[9],
+            series=row[10],
+            price=row[14],
+            price_per_m2=int(row[14] / row[9]),
+            latitude=row[11],
+            longitude=row[12],
+            image_data=row[13]
         )
 
 
 class SS_Flat(Flat):
-    def __init__(self, url: str, district_info: District, raw_info: list[str], img_url: str | None):
+    def __init__(self, url: str, district_name: str, raw_info: list[str], deal_type: str, img_url: str | None):
         self.raw_info = raw_info
         self.img_url = img_url
-        super().__init__(url=url, district=district_info.name,
-                         source=Source.SS, district_info=district_info)
+        super().__init__(url=url, district=district_name,
+                         source=Source.SS, deal_type=deal_type)
 
     def create(self):
         if len(self.raw_info) != 7:
             raise ValueError("Incorrect number of elements in raw_info")
+        self.price = try_parse_int(
+            re.sub(r"[^\d]", "", self.raw_info[6]))
         self.price_per_m2 = try_parse_int(
             re.sub(r"[^\d]", "", self.raw_info[5]))
         self.rooms = try_parse_int(self.raw_info[1])
@@ -112,23 +113,22 @@ class SS_Flat(Flat):
         floors = self.parse_floors(self.raw_info[3])
         self.floor, self.floors_total = floors
         self.series = self.raw_info[4]
-        self.full_price = self.area * float(self.price_per_m2)
         self.id = self.create_id()
 
-    def validate(self):
-        if (self.price_per_m2 > self.district_info.max_price_per_m2):
+    def validate(self, district_info: District):
+        if (self.price / self.area > district_info.max_price_per_m2):
             raise ValueError("Price per m2 is higher than the limit")
-        if (self.price_per_m2 < self.district_info.min_price_per_m2):
+        if (self.price / self.area < district_info.min_price_per_m2):
             raise ValueError("Price per m2 is lower than the limit")
-        if (self.rooms != self.district_info.rooms):
+        if (self.rooms != district_info.rooms):
             raise ValueError("Rooms do not match the settings")
-        if (self.area < self.district_info.min_m2):
+        if (self.area < district_info.min_m2):
             raise ValueError("Area is less than the limit")
-        if (self.floor < self.district_info.min_floor):
+        if (self.floor < district_info.min_floor):
             raise ValueError("Floor is lower than the limit")
         if (self.floors_total == None or self.floor == None):
             raise ValueError("Could not parse floors")
-        if (self.floors_total == self.floor and self.district_info.skip_last_floor):
+        if (self.floors_total == self.floor and district_info.skip_last_floor):
             raise ValueError("Last floor is not allowed")
         if (self.floors_total < self.floor):
             raise ValueError("Last floor is lower than the floor")
