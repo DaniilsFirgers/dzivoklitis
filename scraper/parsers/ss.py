50dbd58d564664027b1820ae7cfd676b70753f04
Daniils Firgers <dfirger@gmail.com>
2025-02-02 20:56:47 +0200

    worjked on ss scraper enhancement

--- scraper/parsers/ss.py
+++ scraper/parsers/ss.py
@@ -2,10 +2,10 @@ import re
 from typing import Dict, List
 from bs4 import BeautifulSoup, ResultSet, Tag
 import requests
-from scraper.config import District, SsParserConfig
+from scraper.config import District, Source, SsParserConfig
 from scraper.core.postgres import Postgres, Type
 from scraper.core.telegram import TelegramBot
-from scraper.flat import Coordinates, Flat, SS_Flat
+from scraper.flat import Coordinates,  SS_Flat
 from scraper.parsers.base import BaseParser
 from scraper.utils.logger import logger
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -15,60 +15,35 @@ from geopy.geocoders import Nominatim
 
 class SSParser(BaseParser):
     def __init__(self, scheduler: BackgroundScheduler, telegram_bot: TelegramBot, postgres: Postgres,
-                 districts: List[District], config: SsParserConfig
+                 preferred_districts: List[District], config: SsParserConfig
                  ):
 
-        super().__init__(config.name, scheduler,
+        super().__init__(Source.SS, scheduler,
                          config.deal_type)
         self.city_name = config.city_name
-        self.districts = districts
+        self.preferred_districts = preferred_districts
         self.look_back_arg = config.timeframe
         self.telegram_bot = telegram_bot
         self.postgres = postgres
 
-    def get_districts_list(self) -> List[int]:
-        districts_found = []
-        url = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/"
+    def scrape(self) -> None:
+        """Scrape flats from SS.lv
+        \n Args:
+            districts (Dict[str, str]): A dictionary of districts with their external keys and names.
+        """
+        # ext_key is centre and district_name is Centrs
+        for ext_key, district_name in self.districts.items():
+            # currently we will check only districts that are in the preferred_districts list
+            district_info = next(
+                (district for district in self.preferred_districts if district.name == district_name), None)
+            if district_info is None:
+                continue
 
-        try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
-        except requests.RequestException as e:
+            # currently we are interested only in a specific deal type (get platform specific deal type by reverse lookup)
+            platform_deal_type = next((k for k, v in self.deal_types.items()
+                                       if v == self.target_deal_type), None)
 
-            raise ValueError(f"Error accessing the website: {e}")
-
-        try:
-            soup = BeautifulSoup(response.text, "html.parser")
-            district_elements = soup.find_all(name="h4", class_="category")
-
-            if not district_elements:
-                raise ValueError("No district elements found on page")
-
-            for index, district in enumerate(district_elements):
-                area: str = district.getText().strip().lower().replace(" ", "-")
-                districts_found.append(area)
-
-                if index == 38:  # Optional limit on number of districts
-                    break
-
-            final_districts = []
-            for district in self.districts:
-                try:
-                    districts_found.index(district.name)
-                    final_districts.append(district)
-                except ValueError:
-                    warning = f"District {district.name} not found"
-                    logger.warning(warning)
-
-            return final_districts
-
-        except Exception as e:
-            raise ValueError(f"Error parsing district data: {e}")
-
-    def scrape(self, districts: List[District]) -> None:
-        flats_found: Dict[str, List[Flat]] = {}
-        for district in districts:
-            pages_url = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/{district.name}/{self.look_back_arg}/{self.deal_type}/"
+            pages_url = f"https://www.ss.lv/en/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{platform_deal_type}/"
             response = requests.get(pages_url)
             bs = BeautifulSoup(response.text, "html.parser")
             all_pages: ResultSet[Tag] = bs.find_all(name="a", class_="navi")
@@ -79,7 +54,7 @@ class SSParser(BaseParser):
                 pages.append(page)
 
             for page in pages:
-                page_url = f"https://www.ss.lv/real-estate/flats/riga/{district.name}/{self.look_back_arg}/{self.deal_type}/page{page}.html"
+                page_url = f"https://www.ss.lv/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{platform_deal_type}/page{page}.html"
                 page_res = requests.get(page_url)
                 bs = BeautifulSoup(page_res.text, "html.parser")
 
@@ -96,13 +71,13 @@ class SSParser(BaseParser):
                     description = descriptions[int(i/7)]
                     info = description.get("href")
                     url = f"https://www.ss.lv/{info}"
-                    id = description.get("id")
 
                     chunk: list[Tag] = streets[i:i + 7]
                     raw_info = [street.get_text() for street in chunk]
                     img_url = self.get_image_url(image_urls, i)
 
-                    flat = SS_Flat(url, district, raw_info, img_url)
+                    flat = SS_Flat(url, district_name, raw_info,
+                                   self.target_deal_type, img_url)
 
                     # Possible options here:
                     # 1. Flat with this id and price is already in the database -> skip
@@ -117,12 +92,12 @@ class SSParser(BaseParser):
                     #   c. Flat with the same parameters (price, area, rooms, etc.) does not exist in the database -> add
 
                     # If flat with the same id and price_per_m2 exists in the database, skip it
-                    if self.postgres.exists_with_id_and_price(flat.id, flat.price_per_m2):
+                    if self.postgres.exists_with_id_and_price(flat.id, flat.price):
                         continue
 
                     try:
-                        flat.validate()
-                    except ValueError:
+                        flat.validate(district_info)
+                    except ValueError as e:
                         continue
 
                     coordinates = self.get_coordinates(
@@ -136,24 +111,13 @@ class SSParser(BaseParser):
 
                     try:
                         self.postgres.add_or_update(flat)
-                        logger.info(f"Added flat {flat.id} to the database")
+                        logger.info(
+                            f"Added flat {flat.id} - {district_name} to the database")
                     except Exception as e:
                         logger.error(e)
                         continue
 
-                    if district.name not in flats_found:
-                        flats_found[district.name] = [flat]
-                    else:
-                        flats_found[district.name].append(flat)
-
-        for district, flats in flats_found.items():
-            msg = f"Found *{len(flats)}* flats in *{district}*"
-            logger.info(msg)
-            self.telegram_bot.send_message(msg)
-
-            for index, flat in enumerate(flats, start=1):
-                counter = f"*{index}/{len(flats)}*"
-                self.telegram_bot.send_flat_message(flat, Type.FLATS, counter)
+                    self.telegram_bot.send_flat_message(flat, Type.FLATS)
 
     def get_image_url(self, image_urls: ResultSet[Tag], i: int) -> str | None:
         if len(image_urls) == 0:
