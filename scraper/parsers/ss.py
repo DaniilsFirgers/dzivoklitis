cda599e89d6bc9b88103e640698a022b435ad8df
Daniils Firgers <dfirger@gmail.com>
2025-02-09 22:19:18 +0200

    big refactoring of the SS scraper

--- scraper/parsers/ss.py
+++ scraper/parsers/ss.py
@@ -1,126 +1,126 @@
-import re
-from typing import Dict, List
+import asyncio
+import aiohttp
+from typing import List
 from bs4 import BeautifulSoup, ResultSet, Tag
-import requests
+
 from scraper.config import District, Source, SsParserConfig
 from scraper.core.postgres import Postgres, Type
 from scraper.core.telegram import TelegramBot
 from scraper.flat import SS_Flat
 from scraper.parsers.base import BaseParser
 from scraper.utils.logger import logger
-from apscheduler.schedulers.background import BackgroundScheduler
-import requests
+from scraper.utils.meta import get_coordinates
 
 
 class SSParser(BaseParser):
-    def __init__(self, scheduler: BackgroundScheduler, telegram_bot: TelegramBot, postgres: Postgres,
+    def __init__(self, telegram_bot: TelegramBot, postgres: Postgres,
                  preferred_districts: List[District], config: SsParserConfig
                  ):
 
-        super().__init__(Source.SS, scheduler,
-                         config.deal_type)
+        super().__init__(Source.SS, config.deal_type)
         self.city_name = config.city_name
         self.preferred_districts = preferred_districts
         self.look_back_arg = config.timeframe
         self.telegram_bot = telegram_bot
         self.postgres = postgres
 
-    def scrape(self) -> None:
-        """Scrape flats from SS.lv
-        \n Args:
-            districts (Dict[str, str]): A dictionary of districts with their external keys and names.
-        """
-        # ext_key is centre and district_name is Centrs
-        for ext_key, district_name in self.districts.items():
-            # currently we will check only districts that are in the preferred_districts list
-            district_info = next(
-                (district for district in self.preferred_districts if district.name == district_name), None)
-            if district_info is None:
-                continue
+    async def fetch_page(self, session: aiohttp.ClientSession, url: str) -> str:
+        try:
+            async with session.get(url) as response:
+                return await response.text()
+        except Exception as e:
+            raise e
 
-            # currently we are interested only in a specific deal type (get platform specific deal type by reverse lookup)
+    async def scrape_district(self, session: aiohttp.ClientSession, ext_key: str, district_name: str):
+        """Scrape all pages of a given district asynchronously with request limits."""
+        async with self.semaphore:
             platform_deal_type = next((k for k, v in self.deal_types.items()
                                        if v == self.target_deal_type), None)
+            base_url = f"https://www.ss.lv/lv/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{platform_deal_type}/"
 
-            pages_url = f"https://www.ss.lv/lv/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{platform_deal_type}/"
-            response = requests.get(pages_url)
-            bs = BeautifulSoup(response.text, "html.parser")
-            all_pages: ResultSet[Tag] = bs.find_all(name="a", class_="navi")
+            first_page_html = await self.fetch_page(session, base_url)
+            bs = BeautifulSoup(first_page_html, "lxml")
+            all_pages: ResultSet[Tag] = bs.find_all("a", class_="navi")
 
             pages = [1]
-            for page_num in all_pages[1:-1]:
-                page = int(page_num.get_text())
-                pages.append(page)
+            pages += [int(page_num.get_text()) for page_num in all_pages[1:-1]]
 
-            for page in pages:
-                page_url = f"https://www.ss.lv/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{platform_deal_type}/page{page}.html"
-                page_res = requests.get(page_url)
-                bs = BeautifulSoup(page_res.text, "html.parser")
+            tasks = [asyncio.create_task(self.scrape_page(session, ext_key, district_name, platform_deal_type, page))
+                     for page in pages]
 
-                descriptions: ResultSet[Tag] = bs.find_all(
-                    name="a", class_="am")
-                streets: ResultSet[Tag] = bs.find_all(
-                    name="td", class_="msga2-o pp6")
-                image_urls: ResultSet[Tag] = bs.find_all(
-                    name="img", class_="isfoto foto_list")
+            await asyncio.gather(*tasks)
 
-                # extracting all data about a flat
-                for i in range(0, len(streets), 7):
-                    # Get a chunk of 7 streets
-                    description = descriptions[int(i/7)]
-                    info = description.get("href")
-                    url = f"https://www.ss.lv/{info}"
+    async def scrape_page(self, session: aiohttp.ClientSession, ext_key: str, district_name: str, deal_type: str, page: int):
+        """Scrape a single page and extract flat details asynchronously."""
+        page_url = f"https://www.ss.lv/real-estate/flats/{self.city_name.lower()}/{ext_key}/{self.look_back_arg}/{deal_type}/page{page}.html"
+        page_html = await self.fetch_page(session, page_url)
 
-                    chunk: list[Tag] = streets[i:i + 7]
-                    raw_info = [street.get_text() for street in chunk]
-                    img_url = self.get_image_url(image_urls, i)
+        bs = BeautifulSoup(page_html, "lxml")
+        descriptions = bs.select("a.am")
+        streets = bs.select("td.msga2-o.pp6")
+        image_urls = bs.select("img.isfoto.foto_list")
 
-                    flat = SS_Flat(url, district_name, raw_info,
-                                   self.target_deal_type)
+        tasks = []
+        for index, (description, streets) in enumerate(zip(descriptions, zip(*[iter(streets)] * 7))):
+            img_url = self.get_image_url(image_urls, index)
+            tasks.append(asyncio.create_task(
+                self.process_flat(description, streets,
+                                  img_url, district_name, session)
+            ))
 
-                    # Possible options here:
-                    # 1. Flat with this id and price is already in the database -> skip
-                    # 2. Flat with this id is already in in the database, but
-                    # the price has changed -> update the price and updated_at and
-                    # add a new flat_price_update record
-                    # 3. Flat with this id is not in the database:
-                    #   a. Flat with the same parameters (price, area, rooms, etc.)
-                    #   exists in the database and price_per_m2 is the same, but with a different id -> update
-                    #   b. Flat with the same parameters (price, area, rooms, etc.) exists in the database,
-                    #   but price_per_m2 is different -> update the price and updated_at and add a new flat_price_update record
-                    #   c. Flat with the same parameters (price, area, rooms, etc.) does not exist in the database -> add
+        await asyncio.gather(*tasks)
 
-                    # If flat with the same id and price_per_m2 exists in the database, skip it
+    async def process_flat(self, description: Tag, streets: tuple[Tag], img_url: str, district_name: str, session: aiohttp.ClientSession):
+        url = f"https://www.ss.lv/{description.get('href')}"
+        raw_info = [street.get_text() for street in streets]
 
-                    try:
-                        flat.create(img_url, self.flat_series)
-                    except Exception as e:
-                        continue
+        flat = SS_Flat(url, district_name, raw_info, self.target_deal_type)
 
-                    if self.postgres.exists_with_id_and_price(flat.id, flat.price):
-                        continue
+        try:
+            flat.create(self.flat_series)
+        except Exception:
+            return
 
-                    try:
-                        flat.validate(district_info)
-                    except ValueError as e:
-                        continue
+        print("flat", flat.id, flat.district, flat.street)
 
-                    try:
-                        self.postgres.add_or_update(flat)
-                        logger.info(
-                            f"Added {Source.SS.value} flat {flat.id} - {district_name} to the database")
-                    except Exception as e:
-                        logger.error(e)
-                        continue
+        flat.image_data = await flat.download_img(img_url, session)
 
-                    self.telegram_bot.send_flat_message(flat, Type.FLATS)
+        await self.telegram_bot.send_flat_message(flat, Type.FLATS)
+
+        # flat.add_coordinates(await get_coordinates(flat.street, self.city_name))
+
+        # if self.postgres.exists_with_id_and_price(flat.id, flat.price):
+        #     return
+
+        # try:
+        #     district_info = next(
+        #         (district for district in self.preferred_districts if district.name == district_name), None)
+        #     if district_info:
+        #         flat.validate(district_info)
+        # except ValueError:
+        #     return
+
+        # try:
+        #     self.postgres.add_or_update(flat)
+        #     logger.info(
+        #         f"Added {Source.SS.value} flat {flat.id} - {district_name} to the database")
+        # except Exception as e:
+        #     logger.error(e)
+
+    async def scrape(self) -> None:
+        connector = aiohttp.TCPConnector(limit_per_host=7)
+        async with aiohttp.ClientSession(connector=connector) as session:
+            tasks = [asyncio.ensure_future(self.scrape_district(session, ext_key, district_name))
+                     for ext_key, district_name in self.districts.items()]
+            await asyncio.gather(*tasks)
 
     def get_image_url(self, image_urls: ResultSet[Tag], i: int) -> str | None:
-        if len(image_urls) == 0:
+        if not image_urls or i >= len(image_urls):
             return None
-        if int(i/7) >= len(image_urls):
+
+        img_url: str = image_urls[i].get("src", "")
+
+        if not img_url:
             return None
-        img_url: str = image_urls[int(i/7)].get("src")
-        # Replace thumbnail with full size image
-        adjusted_img_url = img_url.replace("th2", "800")
-        return adjusted_img_url
+
+        return img_url.replace("th2", "800")
