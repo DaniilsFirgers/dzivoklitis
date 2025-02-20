47ebf96dd4a89858f6b7d6219139eca09c012ae2
Daniils Firgers <dfirger@gmail.com>
2025-02-20 08:24:37 +0200

    updated logger.py

--- scraper/utils/logger.py
+++ scraper/utils/logger.py
@@ -1,11 +1,12 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
         logging.StreamHandler()
     ]
 )
+logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
