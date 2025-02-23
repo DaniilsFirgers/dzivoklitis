0f26bd59674debcb044429275171cf32448965e0
Daniils Firgers <dfirger@gmail.com>
2025-02-23 16:00:42 +0200

    added find_flat_price function

--- scraper/utils/meta.py
+++ scraper/utils/meta.py
@@ -1,11 +1,10 @@
-
-
 import asyncio
 from datetime import datetime, timezone
-from typing import Optional
+from typing import List, Optional
 from fake_useragent import UserAgent
 from geopy.geocoders import Photon
 
+from scraper.database.models import Price
 from scraper.flat import Coordinates
 from scraper.utils.logger import logger
 
@@ -23,6 +22,7 @@ class SingletonMeta(type):
 
 
 def try_parse_int(value: str) -> int:
+    """Try to parse a string to an integer, return 0 if it fails"""
     try:
         return int(value)
     except ValueError:
@@ -30,6 +30,7 @@ def try_parse_int(value: str) -> int:
 
 
 def try_parse_float(value: str) -> float:
+    """Try to parse a string to a float, return 0.0 if it fails."""
     try:
         return float(value)
     except ValueError:
@@ -37,6 +38,7 @@ def try_parse_float(value: str) -> float:
 
 
 def get_start_of_day() -> int:
+    """Get the start of the current day in Unix timestamp."""
     now = datetime.now(timezone.utc).replace(
         hour=0, minute=0, second=0, microsecond=0)
 
@@ -56,7 +58,6 @@ async def get_coordinates(city: str, street: str) -> Optional[Coordinates]:
 
     # Prepare the query
     full_address = f"{street}, {city}, Latvija"
-    print(full_address)
     try:
         # Call async_geocode which uses the ThreadPoolExecutor
         location = await async_geocode(geolocator, full_address)
@@ -72,3 +73,10 @@ async def get_coordinates(city: str, street: str) -> Optional[Coordinates]:
     except Exception as e:
         logger.error(f"Error during geocoding: {e}")
         return None
+
+
+def find_flat_price(curr_price: int, prev_prices: List[Price]) -> Optional[Price]:
+    """Finds the price object from the list of previous prices that matches the current price."""
+    price_dict = {
+        price.price: price for price in prev_prices}
+    return price_dict.get(curr_price)
