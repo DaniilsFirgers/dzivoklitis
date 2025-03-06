d780abffabd55e43df95486b5bee753fbcb42b93
Daniils Firgers <dfirger@gmail.com>
2025-03-06 23:04:46 +0200

    logger.py adjuisted to suppress INFo fro sqlachemy and pyvips + writign ERRORS to a file

--- scraper/utils/logger.py
+++ scraper/utils/logger.py
@@ -1,4 +1,8 @@
 import logging
+import os
+
+log_dir = "/var/log/app"
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -8,7 +12,24 @@ logging.basicConfig(
     ]
 )
 
-logging.getLogger('sqlalchemy.engine').addHandler(logging.StreamHandler())
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# handle sqlachemy logs
+sqlalchemy_logger = logging.getLogger("sqlalchemy.engine")
+sqlalchemy_logger.setLevel(logging.WARNING)
+sqlalchemy_logger.propagate = False  # prevent from propagating to root logger
+sqlalchemy_logger.handlers.clear()
+
+# hand;e pyvips logs
+pyvips_logger = logging.getLogger("pyvips")
+pyvips_logger.setLevel(logging.WARNING)
+pyvips_logger.propagate = False
+pyvips_logger.handlers.clear()
 
 logger = logging.getLogger(__name__)
+
+error_log_path = os.path.join(log_dir, "error.log")
+error_file_handler = logging.FileHandler(error_log_path, mode="w")
+error_file_handler.setLevel(logging.ERROR)
+error_file_handler.setFormatter(logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+
+logger.addHandler(error_file_handler)
