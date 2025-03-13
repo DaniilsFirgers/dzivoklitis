763518b43b1668646e763161d81a837b78cf9942
Daniils Firgers <dfirger@gmail.com>
2025-03-13 23:25:59 +0200

    added city to flats in 001_init.sql table

--- initdb/001_init.sql
+++ initdb/001_init.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS flats(
     deal_type VARCHAR(30) NOT NULL, -- sale or rent
     url TEXT NOT NULL, -- TEXT is a type for long strings
     district VARCHAR(100) NOT NULL, -- VARCHAR is a shorthand for VARCHAR(255), which is a string with a maximum length of 255 characters
+    city VARCHAR(100) NOT NULL,
     street VARCHAR(150) NOT NULL,
     rooms SMALLINT NOT NULL, -- SMALLINT is a type for small integers
     floors_total SMALLINT NOT NULL, -- how many floors are in the building
