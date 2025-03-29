4b33c37696495bad2ad6fb2eb7de057305e2d44d
Daniils Firgers <dfirger@gmail.com>
2025-03-29 20:40:44 +0200

    created varianti schema file

---
+++ scraper/schemas/varianti.py
@@ -0,0 +1,52 @@
+from typing import List, TypedDict
+
+
+class Image(TypedDict):
+    small: str
+    original: str
+
+
+class Object(TypedDict, total=False):
+    area: float
+    price: float
+    price_per_m: float
+    floor: int
+    floors_count: int
+    rooms_count: int
+    flat_building_type: int
+    date_create: int
+
+
+class Address(TypedDict):
+    city_id: int
+    street: str
+    house: str
+    street_id: int
+    apartment: str
+
+
+class Flat(TypedDict, total=False):
+    valid_from: int
+    id: int
+    valid_till: int
+    address_name: str
+    address: Address
+    latitude: float
+    longitude: float
+    deal_type: int
+    category_type: int
+    object: Object
+    images: List[Image]
+
+
+class Result(TypedDict, total=False):
+    pages: int
+    total: int
+    list: List[Flat] | None
+
+
+class VariantiRes(TypedDict, total=False):
+    statusCode: str
+    errorCodes: List[str]
+    errorDescriptions: List[str]
+    result: Result
