8446c71b04f10751e876fba522b99f5085ae4950
Daniils Firgers <dfirger@gmail.com>
2025-04-04 23:28:54 +0300

    created Layout.tsx

---
+++ web-app/src/components/Layout.tsx
@@ -0,0 +1,36 @@
+// src/components/Layout.tsx
+
+import React from "react";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-xl">My Website</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="flex-grow p-4">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
