d7a3946eeddc1a720f08c44dc488205bd7bf1f69
Daniils Firgers <dfirger@gmail.com>
2025-04-06 17:14:01 +0300

    adjusted layout to the new colour pallete

--- web-app/src/components/Layout.tsx
+++ web-app/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 // src/components/Layout.tsx
 
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 
 interface LayoutProps {
@@ -8,23 +9,40 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const location = useLocation();
+  const navItems = [
+    { name: "Sākums", path: "/" },
+    { name: "Favorīti", path: "/favourites" },
+    { name: "Par mums", path: "/about" },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen w-full">
-      <header className="bg-blue-600 text-white p-4">
-        <h1 className="text-xl">My Website</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/favourites">Favorites</a>
-            </li>
-          </ul>
-        </nav>
+      <header className="bg-midnight-indigo border-b border-midnight-azure w-full h-10">
+        <div className="flex justify-between items-center h-full mx-4">
+          <h1 className="text-xl text-indigo-bright">Dzīvoklītis</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <NavLink
+                    to={item.path}
+                    className={`text-md ${
+                      location.pathname == item.path
+                        ? "text-indigo-bright"
+                        : "text-indigo-breeze"
+                    }`}
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </header>
 
-      <main className="flex-grow p-4">{children}</main>
+      <main className="flex-grow p-4 bg-midnight-indigo">{children}</main>
       <Footer />
     </div>
   );
