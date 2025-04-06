e27989c294803153d7027e8f1347fc9e737b7dda
Daniils Firgers <dfirger@gmail.com>
2025-04-06 17:53:08 +0300

    polished up favorites design

--- web-app/src/features/favourite/Favourite.tsx
+++ web-app/src/features/favourite/Favourite.tsx
@@ -1,6 +1,5 @@
 // src/components/Footer.tsx
 import React from "react";
-import Button from "@mui/material/Button";
 import { Favourite as FavouriteType } from "./types";
 
 type Props = {
@@ -12,56 +11,71 @@ const Favourite: React.FC<Props> = ({ favourite }) => {
     <div className="w-fit p-3 rounded-md shadow-md bg-midnight-storm shadow text-md">
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Avots:</b>
-        <h1 className="text-steel-bluee">{favourite.source}</h1>
+        <h1 className="text-steel-blue">{favourite.source}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Pilsēta:</b>
-        <h1 className="text-steel-bluee">{favourite.city}</h1>
+        <h1 className="text-steel-blue">{favourite.city}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Darījums:</b>
-        <h1 className="text-steel-bluee">{favourite.deal_type}</h1>
+        <h1 className="text-steel-blue">{favourite.deal_type}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Apkaime:</b>
-        <h1 className="text-steel-bluee">{favourite.district}</h1>
+        <h1 className="text-steel-blue">{favourite.district}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Iela:</b>
-        <h1 className="text-steel-bluee">{favourite.street}</h1>
+        <h1 className="text-steel-blue">{favourite.street}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Sērija:</b>
-        <h1 className="text-steel-bluee">{favourite.series}</h1>
+        <h1 className="text-steel-blue">{favourite.series}</h1>
       </h2>
       <h2 className="flex ">
         <b className="mr-1.5 text-midnight-moody">Cena €:</b>
-        <h1 className="text-steel-bluee">{favourite.price}</h1>
+        <h1 className="text-steel-blue">{favourite.price}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Cena €/m²:</b>
-        <h1 className="text-steel-bluee"> {favourite.price_per_m2}</h1>
+        <h1 className="text-steel-blue"> {favourite.price_per_m2}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Istabas:</b>
-        <h1 className="text-steel-bluee">{favourite.rooms}</h1>
+        <h1 className="text-steel-blue">{favourite.rooms}</h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Stāvs:</b>
-        <h1 className="text-steel-bluee">
+        <h1 className="text-steel-blue">
           {favourite.floor}/{favourite.floors_total}
         </h1>
       </h2>
       <h2 className="flex">
         <b className="mr-1.5 text-midnight-moody">Platība m²:</b>
-        <h1 className="text-steel-bluee">{favourite.area}</h1>
+        <h1 className="text-steel-blue">{favourite.area}</h1>
       </h2>
-      <div>
-        <Button variant="contained" color="primary">
-          <a href={favourite.url} target="_blank" rel="noopener noreferrer">
+      <div className="pt-2 flex gap-2">
+        <button className="bg-indigo-breeze hover:bg-indigo-breeze/60 px-1.5 py-1 rounded w-[100px]">
+          <a
+            href={favourite.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline text-gray-400"
+          >
             🔍 Aplūkot
           </a>
-        </Button>
+        </button>
+        <button className="bg-red-500/50 hover:bg-red-600/60 px-1.5 py-1 rounded w-[100px]">
+          <a
+            href={favourite.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline text-gray-400"
+          >
+            ❌ Dzēst
+          </a>
+        </button>
       </div>
     </div>
   );
