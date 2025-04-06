17a3c16d3981a77de841f05f92f9931fbc42d4bd
Daniils Firgers <dfirger@gmail.com>
2025-04-06 12:47:40 +0300

    created favourites types file

---
+++ web-app/src/features/favourite/types.ts
@@ -0,0 +1,16 @@
+export type Favourite = {
+  flat_id: string;
+  source: string;
+  deal_type: string;
+  url: string;
+  district: string;
+  street: string;
+  rooms: number;
+  floors_total: number;
+  floor: number;
+  area: number;
+  series: string;
+  city: string;
+  price: number;
+  price_per_m2: number;
+};
